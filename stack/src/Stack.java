6f1f6ed6e19e5bbf9637ede9d193152ecb1c8bc0
Arkoma <aaronburk@Aarons-iMac.local>
2019-02-11 13:05:01 -0600

    Initial code practice

---
+++ stack/src/Stack.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Stack {
+    public String[] storage = new String[1000];
+
+    public String pop() {
+        String lastItem;
+        List<String> storageList = new ArrayList<String>(Arrays.asList(storage));
+
+        return ;
+    }
+}
